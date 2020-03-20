f4f589d7a1de776778b0bac750c56e489432f664
Xiaojin Gu <feibusam@hotmail.com>
2020-03-20 15:31:29 +0100

    RankNet working

--- hw3/ranknet_model.py
+++ hw3/ranknet_model.py
@@ -3,80 +3,18 @@ import torch.nn as nn
 import torch
 
 class RankNet(nn.Module):
-    def __init__(self, input_size, output_size, neg_slope):
+    def __init__(self, input_size, output_size):
         super(RankNet, self).__init__()
 
-        self.sigma = torch.nn.Parameter(torch.tensor(1.))
-        # self.layers = nn.Sequential(
-        #     nn.Linear(input_size, 128, bias=True),
-        #     nn.LeakyReLU(neg_slope),
-        #     nn.BatchNorm1d(128),
-
-        #     nn.Linear(input_size, 512, bias=True),
-        #     nn.LeakyReLU(neg_slope),
-        #     nn.BatchNorm1d(512),
-
-        #     nn.Linear(512, 64, bias=True),
-        #     nn.LeakyReLU(neg_slope),
-        #     nn.BatchNorm1d(64),
-            
-        #     nn.Linear(64, output_size),
-        # )
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, output_size),
+        )
         
-        self.layers = nn.ModuleList()
-
-
-        self.layers.append(nn.Linear(input_size, 128))
-        self.layers.append(nn.ReLU())
-
-        self.layers.append(nn.Linear(128, 64))
-        self.layers.append(nn.ReLU())
-
-        self.layers.append(nn.Linear(64, 32))
-        self.layers.append(nn.ReLU())
-
-        self.layers.append(nn.Linear(32, output_size))        
-
-    def forward(self, x1, x2):
-        """
-        Gets a vector of documents
-        Create all combinations
-        """
-        input = torch.cat((x1, x2))
-
-        # s1 = self.step(x1)
-        # s2 = self.step(x2)
-
-        output = self.step(input)
-        s1, s2 = torch.chunk(output, 2, dim=0)
-
-        # for name, param in self.named_parameters():
-        #     if param.requires_grad:
-        #         print(name, param.data)
-        if torch.isnan(output).sum().item():
-            print(output)
-            raise Exception('Got NaN')
-
-        return s1, s2
-
-    def step(self, input):
-        for layer in self.layers:
-            input = layer.forward(input)
-            if torch.isnan(input).sum().item():
-                print(input)
-        
-        return input
-
-    def weights(self):
-        for name, param in self.named_parameters():
-            if param.requires_grad:
-                print(name, param.data)
-
-
-    def loss(self, S_ij, s1, s2):
-        sig = self.sigma * (s1 - s2)
-        c = 0.5 * (1 - S_ij.view(-1, 1)) * sig + torch.log(1 + torch.exp(-sig))
-
-        if torch.isnan(c).sum().item():
-            print(c.sum())
-        return c.sum()
+    def forward(self, input):
+        return self.layers(input)
--- hw3/ranknet_train.py
+++ hw3/ranknet_train.py
@@ -8,74 +8,76 @@ import datetime
 import torch
 import time
 import itertools
+import matplotlib.pyplot as plt
 
 # CONSTANT PARAMS
 OUTPUT_SIZE = 1
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # HYPER PARAMS
-DEFAULT_BATCH_SIZE = 64
-DEFAULT_LEARNING_RATE = 1e-2
+DEFAULT_LEARNING_RATE = 2e-4
 DEFAULT_EVALUATION_FREQ = 500
 DEFAULT_EPOCHS = 5
-DEFAULT_NEG_SLOP = 0.02
-PRINT_EVERY = 50
+SIGMA = 0.9
+PRINT_EVERY = 100
 
 def main():
     data = dataset.get_dataset().get_data_folds()[0]
     data.read_data()
 
-    # batch the data, skipping last batch
-    X_batched = [data.train.feature_matrix[i:i+DEFAULT_BATCH_SIZE] for i in range(0, len(data.train.feature_matrix), DEFAULT_BATCH_SIZE)][0:-1]
-    y_batched = [data.train.label_vector[i:i+DEFAULT_BATCH_SIZE] for i in range(0, len(data.train.label_vector), DEFAULT_BATCH_SIZE)][0:-1]
-
-    ranknet = RankNet(data.num_features, OUTPUT_SIZE, DEFAULT_NEG_SLOP).to(device)
+    ranknet = RankNet(data.num_features, OUTPUT_SIZE).to(device)
     optimizer = torch.optim.Adam(params=ranknet.parameters(),lr=DEFAULT_LEARNING_RATE)
-    train_loss = []
 
     for epoch in range(1, DEFAULT_EPOCHS+1):
         start = time.time()
-        for i, batch in enumerate(X_batched):
-        # for i in range(len(data.train.feature_matrix)):
-        #     # xj = [data.train.feature_matrix[j] for j in range(i + 1, len(data.train.feature_matrix))]
-        #     for j in range(i + 1, len(data.train.feature_matrix)):
+        batch_start = time.time()
+        for i in range(data.train.num_queries()):
+            train_loss = []
             ranknet.train()
-            batch_start = time.time()
             optimizer.zero_grad()
 
-            # xi = torch.tensor(data.train.feature_matrix[i]).float().to(device)
-            # xj = torch.tensor(data.train.feature_matrix[j]).float().to(device)
-            
-            comb = list(itertools.combinations(batch, 2))
-            x1, x2 = torch.tensor(list(zip(*comb))).float()
+            docs = data.train.query_feat(i)
+            n = len(docs)
+            input = torch.tensor(docs).float()
+            output = ranknet.forward(input)
 
-            s1, s2 = ranknet.forward(x1, x2)
+            if len(docs) >= 2:
+                loss = pairwise_loss(output, data.train.query_labels(i), SIGMA)
+                train_loss.append(loss.item() / (n * (n-1) / 2))
+                loss.backward()
+                optimizer.step()
 
-            S_ij = get_labels(y_batched[i])
-
-            loss = ranknet.loss(S_ij, s1, s2)
-
-            if torch.isnan(loss):
-                raise Exception('Got NaN')
-                continue
-
-            train_loss.append(loss.item())
-
-            loss.backward()
-            optimizer.step()
-            
             if i % PRINT_EVERY == 0:
-                print('TRAIN LOSS [%.2f] | EPOCH [%d] | BATCH [%d / %d] | %.2f seconds' % (loss.item(), epoch, i, len(X_batched), time.time() - batch_start))
-                # print('TRAIN LOSS [{}] | EPOCH [{}] | i = {} j = {} | {} seconds'.format(np.mean(train_loss), epoch, i, j, len(X_batched), time.time() - batch_start))
-
-            if i >= 0 and i % DEFAULT_EVALUATION_FREQ == 0:
+                print('TRAIN LOSS [%.2f] | EPOCH [%d] | BATCH [%d / %d] | %.2f seconds' % (np.mean(train_loss), epoch, i, data.train.num_queries(), time.time() - batch_start))
+                batch_start = time.time()
+        
+            if i > 0 and i % DEFAULT_EVALUATION_FREQ == 0 :
                 test_model(ranknet, data, validation=True)
 
         print('Epoch %i took %f second' % (epoch, time.time() - start))
 
     test_model(ranknet, data, validation=False)
 
+
+def pairwise_loss(output, target, sigma):
+    S_ij = get_labels(target)
+
+    n = output.shape[0]
+    s1 = torch.zeros(int((n * (n - 1) / 2)))
+    s2 = torch.zeros(int((n * (n - 1) / 2)))
+
+    counter = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            s1[counter] = output[i]
+            s2[counter] = output[j]
+            counter += 1
     
+    sig = sigma * (s1 - s2)
+    c = 0.5 * (1 - S_ij.view(-1, 1)) * sig + torch.log(1 + torch.exp(-sig))
+    return c.sum()
+
+
 def get_labels(labels):
     """
     Get labels based on the documents relevance
@@ -97,38 +99,38 @@ def get_labels(labels):
 def test_model(ranknet, data, validation=False):
     ranknet.eval()
     if (validation):
-        X_batched = [data.validation.feature_matrix[i:i+DEFAULT_BATCH_SIZE] for i in range(0, len(data.validation.feature_matrix), DEFAULT_BATCH_SIZE)][0:-1]
-        y_batched = [data.validation.label_vector[i:i+DEFAULT_BATCH_SIZE] for i in range(0, len(data.validation.label_vector), DEFAULT_BATCH_SIZE)][0:-1]
+        dataset = data.validation
     else:
-        X_batched = [data.test.feature_matrix[i:i+DEFAULT_BATCH_SIZE] for i in range(0, len(data.test.feature_matrix), DEFAULT_BATCH_SIZE)][0:-1]
-        y_batched = [data.test.label_vector[i:i+DEFAULT_BATCH_SIZE] for i in range(0, len(data.test.label_vector), DEFAULT_BATCH_SIZE)][0:-1]
+        dataset = data.test
 
     total_loss = 0
     validation_scores = torch.tensor([])
+    results_list = []
 
     with torch.no_grad():
-        for i, batch in enumerate(X_batched):
-            comb = list(itertools.combinations(batch, 2))
-            x1, x2 = torch.tensor(list(zip(*comb))).float()
-            s1, s2 = ranknet.forward(x1, x2)
+        for i in range(dataset.num_queries()):
+            docs = dataset.query_feat(i)
+            n = len(docs)
 
-            S_ij = get_labels(y_batched[i])
+            input = torch.tensor(docs).float()
+            output = ranknet.forward(input)
+            
+            if n >= 2:
+                loss = pairwise_loss(output, dataset.query_labels(i), SIGMA)
+                total_loss += loss.item() / (n * (n - 1) / 2)
 
-            loss = ranknet.loss(labels, S_ij, s1, s2)
-            total_loss += loss.item()
-            validation_scores += s2.clone().detach().numpy()[:,1,:]
+            validation_scores = torch.cat((validation_scores, output.clone().detach().view(-1)))
         
 
-        results = evl.evaluate(labels.numpy(), output.numpy(), print_results=True)
+        results = evl.evaluate(data.validation, validation_scores.numpy(), print_results=True)
+        results_list.append(results)
 
-        total_loss = total_loss / len(X_batched)
+        total_loss = total_loss / dataset.num_queries()
 
         if (validation):
             print("\n", 'AVG VAL LOSS [{}]'.format(total_loss), "\n")
         else:
             print("\n", 'AVG TEST LOSS [{}]'.format(total_loss), "\n")
-
-        return
 
 
 if __name__ == '__main__':
